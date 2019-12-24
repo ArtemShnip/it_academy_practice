81037daa397e0a0b11369cc66c7dc423f42b2721
Artem Shnip <tjoma27@gmail.com>
2019-12-25 01:32:30 +0300

    Task 2 done

--- Solution1/practic_7/Program.cs
+++ Solution1/practic_7/Program.cs
@@ -7,11 +7,12 @@ namespace practic_7
         static void Main(string[] args)
         {
             task1_EmptyArry();
+            task2();
         }
 
         public static void task1_EmptyArry()
         {
-            Console.Write(" Enter length array - ");
+            Console.Write(" Task 1\n Enter length array - ");
             int lenghtArray = int.Parse(Console.ReadLine());
             int[] arrayInt = new int[lenghtArray];
             string[] arrayString = new string[lenghtArray];
@@ -25,6 +26,28 @@ namespace practic_7
             }
             arrayInt[1] = arrayInt[1] + 4;
             Console.WriteLine("\narrayInt[1] + 4 = " + arrayInt[1]);
+        }
+        public static void task2()
+        {
+            Console.Write("\n Press Enter go to task 2");
+            Console.ReadLine();
+            Console.WriteLine(" Task 2");
+            object[] array2 = new object[3];
+            array2[0] = 32;
+            array2[1] = 'A';
+            array2[2] = "Hello";
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine("1)" + array2[i]);
+            }                
+            array2[0] = (int)array2[0] + 10;
+            array2[2] = (string)array2[2] + ", gays!";
+            Console.WriteLine();
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine("2)" + array2[i]);
+            }
         }
     }
 }
