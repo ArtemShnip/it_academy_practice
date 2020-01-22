0e69109be266fd01748bc36ab7836b03ed29155f
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2020-01-22 10:49:17 +0300

    done

---
+++ lecture_05/Lecture_12/Figures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture_12
+{
+    class Figures
+    {
+        public int Side { get; set; }
+
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+
+        public override string ToString()
+        {
+            return $"This is {Name}. CLRType is {Side}. Area is {Area} ";
+        }
+    }
+}
