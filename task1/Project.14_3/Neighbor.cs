345ac0d8db57de28ac7f15a6ff34ae71ded7a9fe
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2020-01-29 22:38:50 +0300

    task 3 done

---
+++ task1/Project.14_3/Neighbor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project._14_3
+{
+    class Neighbor
+    {
+        public string FullName { get; set; }
+        public int FlatNumber { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ task1/Project.14_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project._14_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Neighbor> neighbors = new List<Neighbor>();
+            neighbors.Add(new Neighbor() { FullName = "Ivanov" , FlatNumber =24 , PhoneNumber = 296543215 });
+            neighbors.Add(new Neighbor() { FullName = "Petrov", FlatNumber = 25, PhoneNumber = 296543215 });
+            neighbors.Add(new Neighbor() { FullName = "Sokolov", FlatNumber = 27, PhoneNumber = 296543215 });
+            neighbors.Add(new Neighbor() { FullName = "Kuznecov", FlatNumber = 28, PhoneNumber = 296543215 });
+            Console.WriteLine("Enter flat number");
+            int flat = int.Parse(Console.ReadLine());
+            var selected = from Neighbor in neighbors
+                               where Neighbor.FlatNumber == flat
+                               select Neighbor;
+            foreach (Neighbor neighbor in selected)
+                Console.WriteLine($"Phone: +375{neighbor.PhoneNumber}, name: {neighbor.FullName}");
+        }
+    }
+}
